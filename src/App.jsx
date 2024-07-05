43d488b771590a00015313bc1adda4f105fc5dbe
pjthakur <priyanshjadon@gmail.com>
2024-07-06 01:46:22 +0530

    changes the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { AnimatePresence } from "framer-motion";
+
+import { AnimatePresence, motion } from "framer-motion";
 import './App.css'
-// import MultiStepForm from './components/MultiStepForm'
+
 import Form from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,7 +13,7 @@ function App() {
       <div className='App'>
         <Form/>
       </div>
-      </AnimatePresence>
+    </AnimatePresence>
       
     </>
   )
